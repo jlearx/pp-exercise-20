e7adc99205ab1964f90702087f475fd638b21258
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-08-31 10:23:01 -0700

    Added binary search

--- src/main.py
+++ src/main.py
@@ -27,20 +27,67 @@ def GetList():
 def SlowSearch(numlist, number):
     found = False
     
+    # Search checks each element until it finds the number,
+    # from beginning to end
+    # Potentially searches each element in the list
+    
     for i in numlist:
         if (i == number):
             found = True
+        
+        if (found):
+            break
     
     return found
     
+def BinarySearch(numlist, number):    
+    # List is assumed to be sorted numerically, 
+    # so the search takes advantage of this fact
+
+    return BinSearchRec(numlist, number)
+        
+def BinSearchRec(numlist, number):
+    print(numlist)
+    length = len(numlist)
     
+    # Base case
+    # If the list is empty, we reached end of the branch
+    if (length == 0):
+        return False
+    
+    # Choose the middle of the (sub)list as our guess
+    midIdx = length / 2
+    print(midIdx)
+    middle = numlist[midIdx]
+    
+    # Did we find the number with our guess?
+    if (middle == number):
+        return True
+    elif (middle < number):
+        # The number is larger than our guess
+        # Look on the right of our guess
+        return BinSearchRec(numlist[midIdx + 1:], number)
+    else:
+        # The number is smaller than our guess
+        # Look on the left of our guess        
+        return BinSearchRec(numlist[:midIdx], number)
 
 if __name__ == '__main__':
     numlist = GetList()
     number = int(input("Please enter the number to search for: "))
+    
+    print("Using slow search...")
     found = SlowSearch(numlist, number)
     
     if (found):
         print(str(True) + " - The number is in the list.")
     else:
         print(str(False) + " - The number is not in the list.")
+        
+    print("Using binary search...")
+    found = BinarySearch(numlist, number)
+    
+    if (found):
+        print(str(True) + " - The number is in the list.")
+    else:
+        print(str(False) + " - The number is not in the list.")        
